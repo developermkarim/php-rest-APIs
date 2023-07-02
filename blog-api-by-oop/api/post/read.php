efa8905093c07da2956894a04e8e1425baeb91f6
developermkarim <m.karimcu@gmail.com>
2023-07-03 02:04:59 +0600

    Post data show by Single id done

---
+++ blog-api-by-oop/api/post/read.php
@@ -0,0 +1,47 @@
+<?php
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+// include_once './config/Database.php';
+include_once '../../config/Database.php';
+include_once '../../models/Post.php';
+
+$database = new Database;
+$connect = $database->connect();
+
+// Post Instantiate
+$post = new Post($connect);
+$allPosts = $post->read();
+
+/* Row Count of Fetch Data */
+$rowCount = $allPosts->rowCount();
+if($rowCount > 0){
+
+    /*  Posts array */
+    $post_arr = array();
+    $post_arr['data'] = array();
+    while($row = $allPosts->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+
+        /* Post ITem by Iterating */
+        $post_items = [
+            'id' => $id,
+            'title' => $title,
+            'body' => html_entity_decode($body),
+            'author' => $author,
+            'category_id' => $category_id,
+            'category_name' => $category_name
+        ];
+
+           /* Push to Associative "data" key */
+    // array_push($post_arr['data'],$post_items);
+
+    array_push($post_arr,$post_items);
+
+    }
+ 
+    echo json_encode($post_arr);
+}else{
+    echo json_encode(['message'=>"No Post available"]);
+}
--- blog-api-by-oop/models/Post.php
+++ blog-api-by-oop/models/Post.php
@@ -1,11 +1,10 @@
 <?php
-
 class Post{
     private $conn;
     private $table = "posts";
-
     public $id;
     public $category_id;
+    public $category_name;
     public $title;
     public $body;
     public $author;
@@ -19,44 +18,76 @@ class Post{
     /* Read Data all */
     public function read()
     {
-        $sql = "SELECT c.cateogry_name as category_name, p.category_id, p.title, p.body, p.author, p.created_at" .
-               "FROM " . $this->table . ' p '.
-               " LEFT JOIN categories c 
-               ON c.id = p.category_id 
-               ORDER BY p.id DESC";
+        $sql = 'SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at
+        FROM ' . $this->table . ' p
+        LEFT JOIN
+          categories c ON p.category_id = c.id
+        ORDER BY
+          p.created_at DESC';
                
-               /* Prepare Statement */
-               $stmt = $this->conn->prepare($sql);
-               /* Execute Statement */
-               $stmt->execute();
-               return $stmt;
+               // Prepare statement
+      $stmt = $this->conn->prepare($sql);
+
+      // Execute query
+      $stmt->execute();
+
+      return $stmt;
     }
 
     public function read_single()
     {
-        $sql = "SELECT c.cateogry_name as category_name, p.category_id, p.title, p.body, p.author, p.created_at" .
-        " FROM " . $this->table . " p " .
-        " LEFT JOIN categories c ON p.category_id = c." . 
-        " WHERE id = ?" . 
-        " LIMIT 0,1";
+        $sql = "SELECT c.name as category_name, p.category_id, p.title, p.body, p.author, p.created_at
+         FROM  $this->table p 
+         LEFT JOIN categories c ON p.category_id = c.id  
+         WHERE p.id = ? 
+        LIMIT 0,1";
 
         /* Prepare Statement */
         $stmt = $this->conn->prepare($sql);
         /* Bind Para-meter */
-        $stmt->bindParam('id', $this->id);
+        $stmt->bindParam(1, $this->id);
         /* Statement Execute */
         $stmt->execute();
         /* Data fetching from database */
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
         // Set Properties to Properties
-        
-        $this->id = $row['id'];
+
+        // $this->id = $row['id'];
         $this->title = $row['title'];
         $this->body = $row['body'];
         $this->author = $row['author'];
         $this->category_id = $row['category_id'];
-        $this->created_at = $row['created_at'];
+        $this->category_name = $row['category_name'];
 
+    }
+
+
+    public function create()
+    {
+        $sql = "INSERT INTO $this->table SET title = :title, body = :body, author = :author, category_id = :category_id";
+        $stmt = $this->conn->prepare($sql);
+
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->body = htmlspecialchars($this->body);
+        $this->author = htmlspecialchars($this->author);
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+        /* Bind Data */
+
+        $stmt->bindParam(':title',$this->title);
+        $stmt->bindParam(':body',$this->body);
+        $stmt->bindParam(':author',$this->author);
+        $stmt->bindParam(':category_id',$this->category_id);
+       
+        if($stmt->execute()){
+            return true;
+            
+        }else{
+
+            printf("Error: %s. \n",$stmt->error);
+            return false;
+
+        }
     }
 }
